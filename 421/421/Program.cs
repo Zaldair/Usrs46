4a370de88fbcf1ab1d1b603c25da75515bea7100
ErwanT <temple.erwan@gmail.com>
2022-10-18 11:43:18 +0200

    FirstCommit

---
+++ 421/421/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+
+
+namespace QuatreDeuxUn
+{
+    #region Class
+    #region ClassDe
+    //Class De -------------------------------------------
+    public class De
+    {
+        public string nFace;
+        public Random rand;
+        public int face { get; private set; }
+
+        public De()
+        {
+            this.rand = new Random();
+        }
+        public override string ToString()
+        {
+            string toString = String.Format("+---+  \n");
+            toString += String.Format("| {0} |  \n", nFace);
+            toString += String.Format("+---+  \n");
+            return toString;
+        }
+        public virtual int Lancer()
+        {
+            int rand = this.rand.Next(1, 7);
+            nFace = rand.ToString();
+            return rand;
+        }
+
+    }
+    //Class DeTruque ----------------------------------------
+    public class DeTruque : De
+    {
+        public override string ToString()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            return base.ToString();
+        }
+        public override int Lancer()
+        {
+            int rand = this.rand.Next(1, 13);
+            if (rand > 4 && rand < 7)
+                nFace = "5";
+            else if (rand > 7)
+                nFace = "6";
+            else
+                nFace = rand.ToString();
+            return int.Parse(nFace);
+        }
+    }
+    #endregion
+    #region Jeu
+    public class Jeu
+    {
+        public readonly int nManche;
+        public readonly int nDe;
+        public readonly int nDeTruque;
+        private List<De> des = new List<De>();
+
+        public Jeu(int nManche = 5, int nDe = 5, int nDeTruque = 0)
+        {
+            this.nManche = nManche;
+
+            this.nDe = nDe;
+            for (int i = 0; i < nDe; i++)
+                des.Add(new De());
+
+            this.nDeTruque = nDeTruque;
+            for (int i = 0; i < nDeTruque; i++)
+                des.Add(new DeTruque());
+
+            foreach (De D in des)
+                D.Lancer();
+        }
+
+        public override string ToString()
+        {
+            (int, int) cursorPos = Console.GetCursorPosition();
+            for (int i = 0; i < des.Count; i++)
+            {
+                string[] dString = des[i].ToString().Split('\n');
+                for (int y = 0; y < dString.Length; y++)
+                {
+                    Console.SetCursorPosition(cursorPos.Item1 + 1 + 6 * i, cursorPos.Item2 + y);
+                    Console.Write(dString[y] + "\r");
+                }
+            }
+            Console.ResetColor();
+            return base.ToString();
+        }
+
+        public int Relancer(int index)
+        {
+            return des[index].Lancer();
+        }
+        public int Score()
+        {
+            int score = 0;
+            foreach (De D in des)
+                score += int.Parse(D.nFace);
+            return score;
+        }
+        public string Run()
+        {
+            Console.Write("(" + Score() + " pts) Quel.s dé.s voulez vous relancer ? (a,b,..) : ");
+            string toReturn = Console.ReadLine();
+            return toReturn;
+        }
+    }
+    #endregion
+    #endregion
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Setup
+            Console.Write("En combien de manche voulez vous jouer ? : ");
+            int thisManche = int.Parse(Console.ReadLine());
+            Console.Write("Avec combien de dé voulez vous jouer ? : ");
+            int nDe = int.Parse(Console.ReadLine());
+            Console.Write("Avec combien de dé truqué voulez vous jouer ? : ");
+            int nDeTruque = int.Parse(Console.ReadLine());
+            //Debut de la partie
+            Jeu mainGame = new Jeu(thisManche,nDe,nDeTruque);
+            thisManche = 0;
+            #region BoucleDeJeu
+            while (thisManche < mainGame.nManche)
+            {
+                thisManche++;
+                Console.WriteLine("Manche {0} :", thisManche);
+                mainGame.ToString();
+                string toRoll = mainGame.Run();
+                string[] reroll = toRoll.Split(',');
+                foreach (string s in reroll)
+                {
+                    mainGame.Relancer(int.Parse(s) - 1);
+                }
+            }
+            #endregion 
+            mainGame.ToString();
+        }
+    }
+}
---
+++ StephanePlaza/StephanePlaza/Program.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace StephanePlaza
+{
+    //Class Proprietaire --------------------------------------------
+    public class Proprietaire
+    {
+        public string nom;
+        public string prenom;
+        public Bien[] biens = new Bien[0];
+
+        public Proprietaire(string nom, string prenom, Bien[] biens)
+        {
+            this.nom = nom;
+            this.prenom = prenom;
+            this.biens = biens;
+        }
+        public override string ToString()
+        {
+            string toString = String.Format("{1} {0} {2}", this.nom, this.prenom, this.biens?.Length != 0 ? "possède\n" : "ne possède aucun bien");
+            toString += ListeBiens();
+            return toString;
+        }
+
+        private string ListeBiens()
+        {
+            string listeBiens = "";
+            foreach (Bien B in this.biens)
+            {
+                listeBiens += String.Format("- {0} {1} au {2}\n", B.GetType().Name == "Maison" ? "Une" : "Un", B.GetType().Name, B.adresse);
+            }
+            return listeBiens;
+        }
+
+    }
+    //Class Bien ----------------------------------------------------
+    public class Bien
+    {
+        public string adresse;
+        public float superficie;
+        //Constructeur
+        public Bien(string adresse, float supercifie)
+        {
+            this.adresse = adresse;
+            this.superficie = supercifie;
+        }
+        //string qui est renvoyé quand l'objet est appelé
+        public override string ToString()
+        {
+            string toString = String.Format("Adresse = {0}\n", this.adresse);
+            toString += String.Format("Superficie = {0} m²\n", this.superficie);
+            return toString;
+        }
+        public float EvalutationValeur()
+        {
+            int facteur = 3000;
+            return this.superficie * facteur;
+        }
+    }
+    //Class Maison --------------------------------------------------
+    //L'objet Maison est un enfants de Bien qui est lui le parent
+    public class Maison : Bien
+    {
+        public int nPieces;
+        public bool jardin;
+        public Piece[] pieces;
+        public static readonly List<string> nomPiece = new List<string>() {"Cuisine","Salle de Bain","Chambre","Salon","Garage","Terrasse"};
+        // base fait référence au constructeur du parent (ici Base)
+        public Maison(string adresse, float superficie, int _nPiece, bool _Jardin) : base(adresse,superficie)
+        {
+            this.nPieces = _nPiece;
+            this.jardin = _Jardin;
+            this.superficie = 0;
+            pieces = new Piece[nPieces];
+
+            Random rand = new Random();
+
+            for (int i = 0; i < nPieces; i++)
+            {
+                pieces[i] = new Piece(nomPiece[rand.Next(nomPiece.Count)], rand.Next(10, 30));
+                this.superficie += pieces[i].superficie;
+            }
+        } 
+        public override string ToString()
+        {
+            string toString = String.Format("Nombre de pièces = {0}\n", this.nPieces);
+            for (int i = 0; i < this.nPieces; i++)
+                toString += String.Format("- {0}\n", this.pieces[i]);
+            toString += String.Format("Présence d'un jardin = {0}\n", this.jardin ? "Oui" : "Non");
+            toString += String.Format("> VALEUR = {0}$\n\n", this.EvaluationValeur());
+            return base.ToString()+ toString;
+        }
+        public float EvaluationValeur()
+        {
+            int facteur = 3000;
+
+            if (this.jardin) { facteur += 500; }
+            if (this.nPieces > 3) { facteur += 200; }
+
+            if (Regex.IsMatch(this.adresse, @"\bParis\b")) { facteur += 7000; }
+            else if (Regex.IsMatch(this.adresse, @"\bLyon\b")) { facteur += 2000; }
+
+            return this.superficie * facteur;
+        }
+    }
+    //Class Piece ---------------------------------------------------
+    public class Piece
+    {
+        public string nom;
+        public float superficie;
+
+        public Piece(string nom, float superficie)
+        {
+            this.nom = nom;
+            this.superficie = superficie;
+        }
+        public override string ToString()
+        {
+            string toString = String.Format("La pièce {0} fait {1} m²", this.nom, this.superficie);
+            return toString;
+        }
+    }
+    //Class Terrain -------------------------------------------------
+    public class Terrain : Bien
+    {
+        public int nCoteCloture;
+        public bool riviere;
+
+        public Terrain (string adresse, float superficie, int nCoteCloture, bool riviere) : base(adresse,superficie)
+        {
+            this.nCoteCloture = nCoteCloture;
+            this.riviere = riviere;
+        }
+        public override string ToString()
+        {
+            string toString = String.Format("Nombre de coté = {0}\n", this.nCoteCloture);
+            toString += String.Format("Présence d'une rivière = {0}\n", this.riviere ? "Oui" : "Non");
+            toString += String.Format("> VALEUR = {0}$\n", this.EvaluationValeur());
+            toString += String.Format("> Estimation cout travaux = {0}$\n\n", this.CoutFinirClotures());
+            return base.ToString() + toString;
+        }
+        public new float EvaluationValeur()
+        {
+            int facteur = 3000;
+
+            if (this.riviere) { facteur += 1000; }
+
+            if (Regex.IsMatch(this.adresse, @"\bParis\b")) { facteur += 7000; }
+            else if (Regex.IsMatch(this.adresse, @"\bLyon\b")) { facteur += 2000; }
+
+            return this.superficie * facteur;
+        }
+        public float CoutFinirClotures()
+        {
+            int facteur = 0;
+            facteur += 1000 * nCoteCloture;
+            return facteur;
+        }
+    }
+    #region Main
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Maison maisonUn = new Maison("11 Rue des Chartreux, 69001 Lyon", 58f, 4, false);
+            Maison maisonDeux = new Maison("4 place Saint Louis, 22100 Dinan", 86.5f, 5, true);
+            Maison maisonTrois = new Maison("26 Boulevard Claude Lorrin, 40100 Dax", 25.2f, 2, false);
+
+            Terrain terrainUn = new Terrain("12 Avenue des Coccinelles, 44850 Ligné", 120f, 3, false);
+            Terrain terrainDeux = new Terrain("37 Boulevard Albert Einstein, 75012 Paris", 40f, 5, true);
+            Terrain terrainTrois = new Terrain("14 Route de Manom, 57100 Thionville", 43.7f, 7, true);
+
+            Bien[] agenceImmobiliere = new Bien[]{maisonUn,maisonDeux,maisonTrois, terrainUn, terrainDeux, terrainTrois };
+
+            foreach (Bien B in agenceImmobiliere)
+            {
+                Console.Write(B);
+            }
+
+            Proprietaire Elodie = new Proprietaire("Martin", "Elodie", new Bien[] { maisonUn, terrainUn, terrainTrois });
+            Console.WriteLine(Elodie);
+
+            Proprietaire Marc = new Proprietaire("Dupont", "Marc", new Bien[] {maisonDeux,maisonTrois,terrainDeux });
+            Console.WriteLine(Marc);
+
+            Proprietaire Leo = new Proprietaire("Marin", "Leo", new Bien[0]);
+            Console.WriteLine(Leo);
+        }
+    }
+    #endregion
+}
